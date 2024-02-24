b9038eb20b72c08c41f8abc345ee631e3c3bea16
Kenko Yoshitomi <kyoshitomi.cursor@gmail.com>
2024-02-24 18:10:33 +0900

    Create logic for MainActivity

--- app/src/main/java/com/example/mykeyboard/MainActivity.kt
+++ app/src/main/java/com/example/mykeyboard/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.mykeyboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mykeyboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private fun isKeyboardEnabled(): Boolean {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val enabledInputMethodIds = inputMethodManager.enabledInputMethodList.map { it.id }
+        return enabledInputMethodIds.contains("com.example.mykeyboard/.MyInputMethodService")
+    }
+
+    private fun openKeyboardChooserSettings() {
+        val im = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        im.showInputMethodPicker()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            // check keyboard status
+            if (isKeyboardEnabled())
+                btnEnableKeyboard.isEnabled = false
+            btnEnableKeyboard.setOnClickListener {
+                if (!isKeyboardEnabled())
+                    openKeyboardSettings()
+            }
+            btnChooseKeyboard.setOnClickListener {
+                if (isKeyboardEnabled())
+                    openKeyboardChooserSettings()
+                else Toast.makeText(
+                    this@MainActivity,
+                    "Choose the keyboard activation button",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun openKeyboardSettings() {
+        val intent = Intent(Settings.ACTION_INPUT_METHOD_SETTINGS)
+        startActivity(intent)
     }
 }

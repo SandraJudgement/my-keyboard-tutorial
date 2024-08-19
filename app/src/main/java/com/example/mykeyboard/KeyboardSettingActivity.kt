f5f02a9ddb6247d89abf021523f099ce5e8d554d
SandraJudgement <135982473+SandraJudgement@users.noreply.github.com>
2024-08-20 00:35:17 +0900

    Create KeyboardSettingActivity.kt

---
+++ app/src/main/java/com/example/mykeyboard/KeyboardSettingActivity.kt
@@ -0,0 +1,16 @@
+class KeyboardSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        // ボタン要素（オブジェクト）を取得
+        val buttonToMainActivity = findViewById<Button>(R.id.button_to_main)
+
+        // ボタンタップ時のイベントリスナー
+        buttonToMainActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}

9c1b71b50b9d9d01df14e9a9dd4ac42567e4d497
SandraJudgement <135982473+SandraJudgement@users.noreply.github.com>
2024-08-20 01:12:23 +0900

    Update KeyboardSettingActivity.kt

--- app/src/main/java/com/example/mykeyboard/KeyboardSettingActivity.kt
+++ app/src/main/java/com/example/mykeyboard/KeyboardSettingActivity.kt
@@ -2,6 +2,7 @@ package com.example.mykeyboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.view.View
 import android.widget.Button
 import android.widget.Toast

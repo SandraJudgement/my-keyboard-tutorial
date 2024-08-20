07fa3943327787d82f4b1c9ec149640eb6cb2c90
SandraJudgement <135982473+SandraJudgement@users.noreply.github.com>
2024-08-20 23:16:55 +0900

    Update MyInputMethodService.kt

--- app/src/main/java/com/example/mykeyboard/MyInputMethodService.kt
+++ app/src/main/java/com/example/mykeyboard/MyInputMethodService.kt
@@ -15,8 +15,6 @@ import com.example.mykeyboard.databinding.KeyboardLayoutBinding
 
 class MyInputMethodService : InputMethodService() {
     var nowKeyboardLayout = 0
-    var layout : LinearLayout
-    var targetlayout : LinearLayout
     /*
     override fun onCreate() {
         super.onCreate();
@@ -99,6 +97,7 @@ class MyInputMethodService : InputMethodService() {
 	}
 	keyboardBinding.btnShiftKeyboardLayout.setOnClickListener {
 	    // コンテンツ部分のLayoutを取ってくる
+            var layout : LinearLayout = LinearLayout(null)
             if(nowKeyboardLayout == 0)
                 layout = keyboardBinding.root.findViewById<LinearLayout>(R.id.keylayout_content)
             else if(nowKeyboardLayout == 1)
@@ -108,6 +107,7 @@ class MyInputMethodService : InputMethodService() {
             layout.removeAllViews()
  
             // test_sub.xmlに変更する
+            var targetlayout : LinearLayout
             if(nowKeyboardLayout == 0){
                 getLayoutInflater().inflate(R.layout.gamekeylayout0_content,layout)
 		nowKeyboardLayout = 1

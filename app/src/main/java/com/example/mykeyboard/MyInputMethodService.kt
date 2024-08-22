f4288d314b60a96fb89a0a0fd6744185588b12b1
SandraJudgement <135982473+SandraJudgement@users.noreply.github.com>
2024-08-22 12:46:33 +0900

    Update MyInputMethodService.kt

--- app/src/main/java/com/example/mykeyboard/MyInputMethodService.kt
+++ app/src/main/java/com/example/mykeyboard/MyInputMethodService.kt
@@ -14,6 +14,7 @@ import android.view.MotionEvent
 import com.example.mykeyboard.databinding.KeyboardLayoutBinding
 
 class MyInputMethodService : InputMethodService() {
+    var keyboardBinding : KeyboardLayoutBinding
     var nowKeyboardLayout = 0
     /*
     override fun onCreate() {
@@ -21,7 +22,7 @@ class MyInputMethodService : InputMethodService() {
 	}
     */
     override fun onCreateInputView(): View {
-        val keyboardBinding = KeyboardLayoutBinding.inflate(layoutInflater)
+            keyboardBinding = KeyboardLayoutBinding.inflate(layoutInflater)
 	val aButtonArray = arrayOf(
 	    aButtonData(R.id.btn0,7),aButtonData(R.id.btn1,8),aButtonData(R.id.btn2,9),aButtonData(R.id.btn3,10),aButtonData(R.id.btn4,11),
 	    aButtonData(R.id.btn5,12),aButtonData(R.id.btn6,13),aButtonData(R.id.btn7,14),aButtonData(R.id.btn8,15),aButtonData(R.id.btn9,16),
@@ -136,7 +137,7 @@ class MyInputMethodService : InputMethodService() {
         return keyboardBinding.root
     }
     fun setButtonAction() {
-	var alayout = KeyboardLayoutBinding.root.findViewById<LinearLayout>(R.id.testlayout_content)
+	var alayout = keyboardBinding.root.findViewById<LinearLayout>(R.id.testlayout_content)
 	alayout.btnaaa_Z.setOnClickListener {
             val inputConnection = currentInputConnection
             inputConnection?.sendKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, 8))

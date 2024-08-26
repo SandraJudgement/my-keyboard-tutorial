14ccacf14d7e8d5bf808495ddd28e24b223a7230
SandraJudgement <135982473+SandraJudgement@users.noreply.github.com>
2024-08-26 22:22:28 +0900

    Update MyInputMethodService.kt

--- app/src/main/java/com/example/mykeyboard/MyInputMethodService.kt
+++ app/src/main/java/com/example/mykeyboard/MyInputMethodService.kt
@@ -390,7 +390,7 @@ class MyInputMethodService : InputMethodService() {
 	else{
 	    keylayout_content.background = getDrawable(R.color.white)
 	}
-        Log.d(enableBackgroundTransparency)
+        Log.d(String.valueOf( enableBackgroundTransparency ))
     return true
 }
     /*
@@ -409,7 +409,7 @@ class MyInputMethodService : InputMethodService() {
 	else{
 	    keylayout_content.background = getDrawable(R.color.white)
 	}
-        Log.d(enableBackgroundTransparency)
+        Log.d(String.valueOf( enableBackgroundTransparency ))
     }
     */
 
